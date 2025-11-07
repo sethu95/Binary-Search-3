8a6f46d721fa73e2498527c728dab60d2ea6d4f2
Sethu Manickam <sethu.sxcs@gmail.com>
2025-11-06 21:04:53 -0800

    Implement findClosestElements method in Solution class

---
+++ problem2.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        
+        int low = 0, high = arr.length-k, mid = low + (high-low)/2;
+
+        while(low < high) {
+            mid = low + (high-low)/2;
+            int distS = x - arr[mid];
+            int distE = arr[mid+k] - x;
+
+            if (distS>distE) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+
+        List<Integer> result = new ArrayList<>();
+
+        for (int i=low; i<low+k; i++) {
+            result.add(arr[i]);
+        }
+
+        return result;
+
+    }
+}
